693cc8bff48f2a3f0176f2a2a3d33afcd4da9119
Jay <jaypatel0112@gmail.com>
2024-01-12 15:16:12 -0600

    Initial Commit

---
+++ HW1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW1
+//
+//  Created by Student on 1/10/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
